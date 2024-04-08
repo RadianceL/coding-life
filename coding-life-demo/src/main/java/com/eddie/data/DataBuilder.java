ced4150b6606745a98c61d6db643f13139c3a1ff
eddie.lys <ysh.li@misumi.sh.cn>
2024-04-08 11:39:35 +0800

    update: netty

--- coding-life-demo/src/main/java/com/eddie/data/DataBuilder.java
+++ coding-life-demo/src/main/java/com/eddie/data/DataBuilder.java
@@ -17,8 +17,5 @@ public class DataBuilder {
     public DataBuilder(String name, String from) {
         this.name = name;
         this.from = from;
-        Throwable cause = new Throwable();
-        StackTraceElement[] stackTrace = cause.getStackTrace();
-        stackTrace[0].
     }
 }
